418222f36e523409507455fa9d2da9d4cc28f740
Jed Walton <jedwaltondev@gmail.com>
2024-02-24 18:12:30 +0000

    +

--- server_utils/start_server.sh
+++ server_utils/start_server.sh
@@ -1,5 +1,4 @@
-- Start the server with the following command:
-```$ sudo docker-compose up --build -d```
-- Stop the server with the following command:
-```$ sudo docker-compose down```
+#!/bin/bash
+
+sudo docker-compose up --build -d
 
