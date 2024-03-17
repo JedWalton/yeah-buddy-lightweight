415595e4884fc77e387393d2b738a06402dfd1e2
Jed Walton <jedwaltondev@gmail.com>
2024-03-17 12:54:06 +0000

    Optimize database settings and backup script
    The docker-compose configuration files have been adjusted, moving the PostgreSQL URL to a new .env.production.local.example file. The backup_db.sh script has been revised, thereby improving the way it gets database connection variables from the .env file and enhancing the way it implements the pg_dump command. This simplifies and enhances the process for backing up the database.

--- server_utils/backup_db.sh
+++ server_utils/backup_db.sh
@@ -2,16 +2,27 @@
 
 # Load environment variables from .env file in project root
 set -a
-source ../.env
+source ./.env.production
 set +a
+
+# Extract the components from POSTGRESQL_URL
+PROTO="$(echo $POSTGRESQL_URL | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+URL="$(echo ${POSTGRESQL_URL/$PROTO/})"
+USER="$(echo $URL | grep @ | cut -d: -f1)"
+PASS="$(echo $URL | grep @ | cut -d@ -f1 | cut -d: -f2)"
+HOST="$(echo ${URL/$USER:$PASS@/} | cut -d: -f1)"
+PORT="$(echo ${URL/$USER:$PASS@/} | cut -d: -f2 | cut -d/ -f1)"
+DB="$(echo $URL | grep / | cut -d/ -f2-)"
+
+# Prompt for password to avoid showing it in the command
+export PGPASSWORD=$PASS
 
 # Set the date format and backup directory
 BACKUP_DATE=$(date +%Y%m%d%H%M%S)
-BACKUP_DIR="../db.bak"  # Ensure this directory exists
+BACKUP_DIR="./database_backups"  # Ensure this directory exists
 
-# Create a backup
-pg_dump -h localhost -p 5432 -U $POSTGRES_USER -d $POSTGRES_DB > "$BACKUP_DIR/db-backup-$BACKUP_DATE.sql"
+# Execute pg_dump
+pg_dump -U "$USER" -h "$HOST" -p "$PORT" -d "$DB" > "$BACKUP_DIR/db-backup-$BACKUP_DATE.sql"
 
-
-echo "Backup created at $BACKUP_DIR/db-backup-$BACKUP_DATE.sql"
-
+# Clear the password variable
+unset PGPASSWORD
