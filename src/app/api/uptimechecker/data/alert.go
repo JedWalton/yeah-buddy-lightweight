dc312408de0025b759c0432261e1132dd2aef757
Jed <jedwaltondev@gmail.com>
2024-06-11 21:20:54 +0100

    remove unused package

--- src/app/api/uptimechecker/data/alert.go
+++
@@ -1,8 +0,0 @@
-package uptimechecker
-
-// Alert Log Management
-func (r *Repository) RecordAlert(channelId, endpointId int, message string) error {
-	query := `INSERT INTO Alerts (channel_id, endpoint_id, message, sent_at) VALUES ($1, $2, $3, NOW())`
-	_, err := r.DB.Exec(query, channelId, endpointId, message)
-	return err
-}
